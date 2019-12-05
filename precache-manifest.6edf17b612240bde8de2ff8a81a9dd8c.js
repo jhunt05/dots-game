4465964820231e4d2bc811f0d515b9fd2e01c056
Viktoriia <jhunt@ukr.net>
2019-12-05 14:27:38 +0200

    Updates

---
+++ precache-manifest.6edf17b612240bde8de2ff8a81a9dd8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b907a9b4cd6128ea523e5fef814d65",
+    "url": "/test_starnavi/index.html"
+  },
+  {
+    "revision": "ac67cc27ef5447bbfa3a",
+    "url": "/test_starnavi/static/css/main.195c5d92.chunk.css"
+  },
+  {
+    "revision": "cab638c4c572b8f49a9c",
+    "url": "/test_starnavi/static/js/2.87e44ece.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/test_starnavi/static/js/2.87e44ece.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac67cc27ef5447bbfa3a",
+    "url": "/test_starnavi/static/js/main.6cd9f18f.chunk.js"
+  },
+  {
+    "revision": "899cab49393c9d0de02c",
+    "url": "/test_starnavi/static/js/runtime-main.7ca76666.js"
+  }
+]);
